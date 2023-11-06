75716089c4be564a797cb30c44bd336a2a198045
Jafar Juneidi <jafar@labiba.ai>
2023-11-06 11:17:19 +0200

    use Node binary

--- Integrty.js
+++ Integrty.js
@@ -1,8 +1,10 @@
 (async function () {
   // Define the calculateIntegrityValue function
-  async function calculateIntegrityValue(url) {
-    const response = await fetch(url);
-    const fileContent = await response.text();
+  const fs = require('fs').promises;
+  const crypto = require('crypto');
+
+  async function calculateIntegrityValue(filePath) {
+    const fileContent = await fs.readFile(filePath, 'utf8');
 
     const encoder = new TextEncoder();
     const data = encoder.encode(fileContent);
@@ -17,14 +19,14 @@
   }
 
   // Example usage
-  const scriptUrl = 'https://cdn.labiba.ai/static/js/bundle-Absher-Business.js';
-  const scriptUrlEn = 'https://cdn.labiba.ai/static/js/bundle-Absher-Business-en.js';
+  const scriptUrl = '../absher_business_ar/build/static/js/bundle-Absher-Business.js';
+  const scriptUrlEn = '../absher_business_en/build/static/js/bundle-Absher-Business-en.js';
 
   const integrity = await calculateIntegrityValue(scriptUrl);
   const integrityEn = await calculateIntegrityValue(scriptUrlEn);
 
-  console.log(scriptUrl + " Ar " + integrity);
-  console.log(scriptUrlEn + " En " + integrityEn);
+  console.log("Ar: " + integrity);
+  console.log("En: " + integrityEn);
 
   // Rest of your startup code goes here
   // ...
