bb0d558b99c9e861903853eddab33914f93c933a
ObadaLabiba <o.almashni@labiba.ai>
2025-01-05 14:27:04 +0300

    update on code

--- Integrty.js
+++ Integrty.js
@@ -1,34 +1,26 @@
 (async function () {
-  // Define the calculateIntegrityValue function
   const fs = require('fs').promises;
   const crypto = require('crypto');
 
   async function calculateIntegrityValue(filePath) {
-    const fileContent = await fs.readFile(filePath, 'utf8');
+    const fileContent = await fs.readFile(filePath);
 
-    const encoder = new TextEncoder();
-    const data = encoder.encode(fileContent);
+    const hash = crypto.createHash('sha256').update(fileContent).digest('base64');
 
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-    // const hashArray = Array.from(new Uint8Array(hashBuffer));
-    // const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-    const hashBase64 = btoa(String.fromCharCode(...new Uint8Array(hashBuffer)));
-
-
-    return `sha256-${hashBase64}`;
+    return `sha256-${hash}`;
   }
 
-  // Example usage
-  const scriptUrl = '../absher_business_ar/build/static/js/bundle-Absher-Business.js';
-  const scriptUrlEn = '../absher_business_en/build/static/js/bundle-Absher-Business-en.js';
+  try {
+    const scriptUrl = '../Labiba Bundle/absher_business_ar/build/static/js/bundle-Absher-Business.js';
+    const scriptUrlEn = '../Labiba Bundle/absher_business_en/build/static/js/bundle-Absher-Business-en.js';
 
-  const integrity = await calculateIntegrityValue(scriptUrl);
-  const integrityEn = await calculateIntegrityValue(scriptUrlEn);
+    const integrity = await calculateIntegrityValue(scriptUrl);
+    const integrityEn = await calculateIntegrityValue(scriptUrlEn);
 
-  console.log("Ar: " + integrity);
-  console.log("En: " + integrityEn);
+    console.log("Ar: " + integrity);
+    console.log("En: " + integrityEn);
 
-  // Rest of your startup code goes here
-  // ...
-
+  } catch (error) {
+    console.error('Error calculating integrity values:', error);
+  }
 })();
