d9e23c6449107d5e0ca8e8e8ffea3279fce08010
ObadaLabiba <o.almashni@labiba.ai>
2025-01-05 16:22:56 +0300

    add featcher Link

--- Integrty.js
+++ Integrty.js
@@ -1,13 +1,33 @@
 (async function () {
   const fs = require('fs').promises;
   const crypto = require('crypto');
+  const axios = require('axios');
+  const path = require('path');
 
-  async function calculateIntegrityValue(filePath) {
-    const fileContent = await fs.readFile(filePath);
+  async function calculateIntegrityValue(input) {
+    let fileContent;
+
+    if (isValidUrl(input)) {
+      console.log(`Fetching from URL: ${input}`);
+      const response = await axios.get(input, { responseType: 'arraybuffer' });
+      fileContent = response.data; 
+    } else {
+      console.log(`Reading from file path: ${input}`);
+      fileContent = await fs.readFile(input);
+    }
 
     const hash = crypto.createHash('sha256').update(fileContent).digest('base64');
 
     return `sha256-${hash}`;
+  }
+
+  function isValidUrl(string) {
+    try {
+      new URL(string);
+      return true;
+    } catch (_) {
+      return false;
+    }
   }
 
   try {
@@ -19,8 +39,7 @@
 
     console.log("Ar: " + integrity);
     console.log("En: " + integrityEn);
-
   } catch (error) {
-    console.error('Error calculating integrity values:', error);
+    console.error('Error calculating integrity values:', error.message);
   }
 })();
